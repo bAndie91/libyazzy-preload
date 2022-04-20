3d1a5932297c8b6cd6acc9fcb5876c1db5a66a00
Andreas Hrubak <ad132454@uucp.hu>
2022-04-21 01:25:36 +0200

    footgun was shot

--- src/xopenhook.c
+++ src/xopenhook.c
@@ -13,6 +13,14 @@ Display* XOpenDisplay(const char * display_name)
 {
 	Display* (*real_XOpenDisplay)(const char *) = dlsym(RTLD_NEXT, "XOpenDisplay");
 	Display* display_handle = real_XOpenDisplay(display_name);
-	if(display_handle != NULL) system(getenv("XOPENHOOK_COMMAND"));
+	
+	char * xopenhook_enable = getenv("XOPENHOOK_ENABLE");
+	if(xopenhook_enable != NULL && strcmp(xopenhook_enable, "1")==0)
+	{
+		unsetenv("XOPENHOOK_ENABLE");
+		if(display_handle != NULL) system(getenv("XOPENHOOK_COMMAND"));
+		setenv("XOPENHOOK_ENABLE", xopenhook_enable, 1);
+	}
+	
 	return display_handle;
 }
