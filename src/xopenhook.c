d62c60b0886dede5dfdc5cb15c3565ce8b873214
Andreas Hrubak <ad132454@uucp.hu>
2022-04-29 15:40:21 +0200

    xopenhook captures XvListImageFormats as well because xfreerdp gets an X11 exception if the display is not on the currently active VT during XvListImageFormats call

--- src/xopenhook.c
+++ src/xopenhook.c
@@ -9,18 +9,42 @@
 #include <sys/stat.h>
 #include <X11/Xlib.h>
 
-Display* XOpenDisplay(const char * display_name)
+void _xopenhook_preload_run(const char * phase)
 {
-	Display* (*real_XOpenDisplay)(const char *) = dlsym(RTLD_NEXT, "XOpenDisplay");
-	Display* display_handle = real_XOpenDisplay(display_name);
-	
 	char * xopenhook_enable = getenv("XOPENHOOK_ENABLE");
 	if(xopenhook_enable != NULL && strcmp(xopenhook_enable, "1")==0)
 	{
 		unsetenv("XOPENHOOK_ENABLE");
-		if(display_handle != NULL) system(getenv("XOPENHOOK_COMMAND"));
+		setenv("XOPENHOOK_PHASE", phase, 1);
+		system(getenv("XOPENHOOK_COMMAND"));
+		unsetenv("XOPENHOOK_PHASE");
 		setenv("XOPENHOOK_ENABLE", xopenhook_enable, 1);
 	}
+}
+
+Display* XOpenDisplay(const char * display_name)
+{
+	Display* (*real_XOpenDisplay)(const char *) = dlsym(RTLD_NEXT, "XOpenDisplay");
+	
+	Display* display_handle = real_XOpenDisplay(display_name);
+	if(display_handle != NULL) _xopenhook_preload_run("post-XOpenDisplay");
 	
 	return display_handle;
+}
+
+
+#include <Xvlibint.h>
+
+XvImageFormatValues * XvListImageFormats (
+   Display 	*dpy,
+   XvPortID 	port,
+   int 		*num
+){
+	XvImageFormatValues* (*real_XvListImageFormats)(Display*, XvPortID, int*) = dlsym(RTLD_NEXT, "XvListImageFormats");
+
+	_xopenhook_preload_run("pre-XvListImageFormats");
+	XvImageFormatValues *ret = real_XvListImageFormats(dpy, port, num);
+	_xopenhook_preload_run("post-XvListImageFormats");
+
+	return ret;
 }
